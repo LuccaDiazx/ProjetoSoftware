a7bd752639087c9ebe8f9257ea85c02aa4102d65
Diaazx <lucca.dias.prosdocimi@gmail.com>
2025-03-14 19:43:13 -0300

    Desafios

---
+++ Desafios/src/Desafios/Desafios1.java
@@ -0,0 +1,23 @@
+package Desafios;
+import java.util.Scanner;
+import java.util.Random;
+public class Desafios1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Random random = new Random();
+		
+		int  NumeroMisterioso =random.nextInt(10)+1;
+		System.out.println("Adivinhe o NUmero Misterioso:");
+		
+		int palpite = sc.nextInt();
+		
+		if (palpite == NumeroMisterioso ) {
+			System.out.println("Parabéns!Você Acertou  o número Misterioso!");
+		}else{
+			System.out.println("Você errou o número Misterioso,o número era :"+NumeroMisterioso);
+		}
+		sc.close();
+	}
+
+}
---
+++ Desafios/src/Desafios/Desafios2.java
@@ -0,0 +1,15 @@
+package Desafios;
+import java.util.Scanner;
+public class Desafios2 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite um valor em real(R$):");
+		double valorReais=sc.nextDouble();
+		
+		System.out.println("");
+		
+		
+	}
+
+}
