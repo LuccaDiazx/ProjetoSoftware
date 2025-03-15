37d2711e9c99dbbf20a8b8cb7ca202027102882b
Diaazx <lucca.dias.prosdocimi@gmail.com>
2025-03-14 23:01:37 -0300

    Desafios 2 e 3

--- Desafios/src/Desafios/Desafios2.java
+++ Desafios/src/Desafios/Desafios2.java
@@ -7,9 +7,37 @@ public class Desafios2 {
 		System.out.println("Digite um valor em real(R$):");
 		double valorReais=sc.nextDouble();
 		
-		System.out.println("");
+		System.out.println("Escolha a moeda para a qual deseja converter:");
+		System.out.println("1.Dolar(USD)");
+		System.out.println("2.Euro(EUR)");
+		System.out.println("3.Libra Esterlina(GBP)");
+		System.out.println("Digite a opção que deseja:");
+		int Moeda=sc.nextInt();
 		
+		double valorConvertido=0;
+		String moeda="";
 		
+		switch (Moeda) {
+		
+		case 1:
+			valorConvertido = valorReais *0.20;
+			moeda="Dólares(USD)";
+		break;
+		case 2:
+			valorConvertido=valorReais*0.18;
+			moeda="Euros(EUR)";
+		break;
+		case 3:
+			valorConvertido=valorReais*0.15;
+			moeda="Libras Esterlinas(GBP)";
+		break;
+		default:
+			System.out.println("Opção Inválida!");
+			return;
+		}
+		System.out.printf("R$%.2f equivale %2f %s",valorReais ,valorConvertido,moeda);
+		
+		sc.close();
 	}
 
 }
---
+++ Desafios/src/Desafios/Desafios3.java
@@ -0,0 +1,10 @@
+package Desafios;
+
+public class Desafios3 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
