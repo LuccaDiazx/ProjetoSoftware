2353c4a247305343fdd1ba6539a1889dee9ea346
Diaazx <lucca.dias.prosdocimi@gmail.com>
2025-04-07 00:02:17 -0300

    .

--- Aula7-Encapsulamento/src/Main/Main.java
+++ Aula7-Encapsulamento/src/Main/Main.java
@@ -1,10 +1,21 @@
 package Main;
+import Entidades.ContaBancaria;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		ContaBancaria Ph=new ContaBancaria("Ph","0604");
+		ContaBancaria Maria =new ContaBancaria("Maria",01801");
+		
+		Ph.setTitular("Pedro Henrique ");
+		Ph.setSaldo(500);
+		
+		Ph.Depositar(250);
+		Ph.Saque(1000);
+		
+		
+		System.out.println("Nome do Titular "+Ph.getTitular());
+		System.out.println("Saldo"+Ph.getSaldo());
 	}
 
 }
--- DesafiosMetodos/src/DesafioRoupas/Roupas.java
+++ DesafiosMetodos/src/DesafioRoupas/Roupas.java
@@ -8,7 +8,44 @@ public String Tamanho;
 public int Quantidade;
 public double Valor;
 
+////Getters e Setters
+
+public String getMarca() {
+	return Marca;
+}
+public void setMarca(String Marca) {
+	this.Marca=Marca;
+}
+public String getTipo() {
+	return Tipo;
+}
+public void setTipo(String Tipo) {
+	this.Tipo=Tipo;
+}
+public String getTamanho() {
+	return Tamanho;
+}
+public void setTamanho(String Tamanho) {
+	this.Tamanho= Tamanho;
+}
+public int getQuantidade(){
+	return Quantidade;
+}
+public void setQuantidade(int Quantidade) {
+	this.Quantidade = Quantidade;
+}
+
+public double getValor() {
+	return Valor;
+}
+public void setValor(double Valor) {
+	this.Valor=Valor;
+	
+}
+
+
 public void Decrementa(int Quantidade) {
+	
 	
 	if(Quantidade>this.Quantidade) {
 		System.out.println("Não é possível remover mais do que o disponível no Estoque!!");
